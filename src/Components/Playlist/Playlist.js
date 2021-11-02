2b7df95efa74949113582c9bffc549d62e0f16e4
Jason Reader <36717169+readerjt@users.noreply.github.com>
2021-11-02 17:36:24 -0400

    Added functionality to add and remove songs from playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './Playlist.css';
 
+import TrackList from '../TrackList/TrackList';
+
 class Playlist extends React.Component {
     render() {
         return (
             <div class="Playlist">
                 <input defaultValue={"New Playlist"}/>
-                {/* <TrackList /> */}
+                <TrackList tracks={this.props.playlistTracks}
+                  onRemove={this.props.onRemove}
+                  isRemoval={true}/>
                 <button class="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './SearchResults.css';
 
-import '../TrackList/TrackList'
+import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/* <TrackList /> */}
+                <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false}/>
             </div>
         )
     }
